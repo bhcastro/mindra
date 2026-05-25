38c7c47cf656a1b903be46a9d1daa6c6d2dd22d8
Bruno Castro <143843806+bhcastro@users.noreply.github.com>
2026-05-25 12:45:33 -0300

    Refactor calculations and improve output handling

--- evidya/js/controles.js
+++ evidya/js/controles.js
@@ -1,4 +1,4 @@
-        // Exibição dos dados selecionados
+    // Declarações
 
 const inputSetor = document.getElementById('seletor-setores');
 const outputSetor = document.getElementById('output-setor-controle');
@@ -12,6 +12,26 @@ const outputLote = document.getElementById('output-lote-controle');
 const inputAnalito = document.getElementById('seletor-analitos');
 const outputAnalito = document.getElementById('output-analito-controle');
 
+const outputMediaN1 = document.getElementById('media-n1');
+const outputMediaN2 = document.getElementById('media-n2');
+
+const controleN1 = document.getElementById('controle-n1');
+const controleN2 = document.getElementById('controle-n2');
+
+const listaControleN1 = [];
+let listaDpN1 = [];
+
+const listaControleN2 = [];
+let listaDpN2 = [];
+
+const outputDp1N1 = document.getElementById('dp-1-n1');
+const outputDp2N1 = document.getElementById('dp-2-n1');
+const outputDp3N1 = document.getElementById('dp-3-n1');
+
+const btnSalvar = document.getElementById('btn-salvar');
+btnSalvar.addEventListener('click', pegarValores);
+
+    // Exibição dos dados selecionados
 
 inputSetor.addEventListener('change', () => {
     let setor = inputSetor.value;
@@ -35,17 +55,6 @@ inputAnalito.addEventListener('change', () => {
 
 
         // Cálculos
-const btnSalvar = document.getElementById('btn-salvar');
-
-const controleN1 = document.getElementById('controle-n1');
-const controleN2 = document.getElementById('controle-n2');
-
-const listaControleN1 = [];
-let listaDpN1 = [];
-const listaControleN2 = [];
-let listaDpN2 = [];
-
-btnSalvar.addEventListener('click', pegarValores);
 
 function pegarValores() {
     let nivel1 = controleN1.value.replace(",", ".");
@@ -62,59 +71,34 @@ function pegarValores() {
         listaControleN2.push(nivel2);
         controleN1.value = "";
         controleN2.value = "";
-        console.log("Nível 1:", listaControleN1);
-        console.log("Nível 2:", listaControleN2);
-        calcularMedia(listaControleN1);
+      if(listaControleN1.length >= 2 && listaControleN2.length >= 2){
+        calcularMedia(listaControleN1, outputMediaN1);
+        //calcularMedia(listaControleN2, outputMediaN2);
+      }
     }
 }
 
-function calcularMedia(listaN1) {
-    let mediaN1 = listaN1.reduce((acc, valor) => acc + parseFloat(valor), 0) / listaN1.length;
-    mediaN1 = mediaN1.toFixed(2).replace(".", ",");
-
-    let mediaN2 = listaN2.reduce((acc, valor) => acc + parseFloat(valor), 0) / listaN2.length;
-    mediaN2 = mediaN2.toFixed(2).replace(".", ",");
-    
-    if(listaControleN1.length >= 3 && listaControleN2.length >= 3) {
-    exibirMedia(mediaN1, mediaN2);
-    calcularDP(mediaN1, listaControleN1);
+function calcularMedia(listaControle, outputMedia) {
+  let mediaListaControle = listaControle.reduce((acc, valor) => acc + parseFloat(valor), 0) / listaControle.length;
+  mediaListaControle = mediaListaControle.toFixed(2);
+  outputMedia.textContent = mediaListaControle.replace(".", ",");
+    calcularDp(mediaListaControle, listaControle, listaDpN1);
     //calcularDP(mediaN2, listaControleN2);
-
-    }
 }
 
-function calcularDP(media, listaControle, listaDp){
+function calcularDp(media, listaControle, listaDp){
     listaDp.length = 0;
 
     for(let i = 0; i < listaControle.length; i++) {
         let valorDiferenca = (listaControle[i] - media)**2;
         listaDp.push(valorDiferenca);
     }
-    let mediaDiferenca = listaDp.reduce((acc, i) => acc + i, 0) / listaDp.length;
-    let dpFinal = Math.sqrt(mediaDiferenca).toFixed(2).replace(".", ",");
+    let mediaDiferenca = listaDp.reduce((acc, i) => acc + i, 0) / (listaDp.length -1);
+    let dpFinal = Math.sqrt(mediaDiferenca).toFixed(2);
 
     if(listaControle.length >= 3){
-        exibirDP(dpFinal);
+      outputDp1N1.textContent = dpFinal.replace(".", ",");
+      outputDp2N1.textContent = (dpFinal * 2).toFixed(2).replace(".", ",");
+      outputDp3N1.textContent = (dpFinal * 3).toFixed(2).replace(".", ","); 
     }
-}
-
-        //Exibição dos resultados
-
-const outputMediaN1 = document.getElementById('media-n1');
-const outputMediaN2 = document.getElementById('media-n2');
-
-
-function exibirMedia(mediaN1, mediaN2) {
-    outputMediaN1.textContent = mediaN1;
-    //outputMediaN2.textContent = mediaN2;
-}
-
-function exibirDP(dp) {
-    const outputDp1N1 = document.getElementById('dp-1-n1');
-    const outputDp2N1 = document.getElementById('dp-2-n1');
-    const outputDp3N1 = document.getElementById('dp-3-n1');
-
-    outputDp1N1.textContent = dp;
-    outputDp2N1.textContent = (dp * 2).toFixed(2).replace(".", ",");
-    outputDp3N1.textContent = (dp * 3).toFixed(2).replace(".", ",");
 }
