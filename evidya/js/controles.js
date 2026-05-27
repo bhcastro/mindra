fb08d4039fb86bb8f63c160c4c00f17c7a4aee95
Bruno Castro <bruno.hen.castro@gmail.com>
2026-05-27 17:03:31 -0300

    Ajustes de UI/UX

--- evidya/js/controles.js
+++ evidya/js/controles.js
@@ -1,4 +1,4 @@
-    // Declarações
+// Declarações
 
 const inputSetor = document.getElementById('seletor-setores');
 const outputSetor = document.getElementById('output-setor-controle');
@@ -15,8 +15,8 @@ const outputAnalito = document.getElementById('output-analito-controle');
 const outputMediaN1 = document.getElementById('media-n1');
 const outputMediaN2 = document.getElementById('media-n2');
 
-const controleN1 = document.getElementById('controle-n1');
-const controleN2 = document.getElementById('controle-n2');
+const inputControleN1 = document.getElementById('input-controle-n1');
+const inputControleN2 = document.getElementById('input-controle-n2');
 
 const listaControleN1 = [];
 let listaDpN1 = [];
@@ -27,11 +27,19 @@ let listaDpN2 = [];
 const outputDp1N1 = document.getElementById('dp-1-n1');
 const outputDp2N1 = document.getElementById('dp-2-n1');
 const outputDp3N1 = document.getElementById('dp-3-n1');
+const outputCvN1 = document.getElementById('cv-n1');
+const outputTotalPontosN1 = document.getElementById('total-pontos-n1');
+
+const outputDp1N2 = document.getElementById('dp-1-n2');
+const outputDp2N2 = document.getElementById('dp-2-n2');
+const outputDp3N2 = document.getElementById('dp-3-n2');
+const outputCvN2 = document.getElementById('cv-n2');
+const outputTotalPontosN2 = document.getElementById('total-pontos-n2');
 
 const btnSalvar = document.getElementById('btn-salvar');
 btnSalvar.addEventListener('click', pegarValores);
 
-    // Exibição dos dados selecionados
+// Exibição dos dados selecionados
 
 inputSetor.addEventListener('change', () => {
     let setor = inputSetor.value;
@@ -54,51 +62,51 @@ inputAnalito.addEventListener('change', () => {
 });
 
 
-        // Cálculos
+// Cálculos
 
 function pegarValores() {
-    let nivel1 = controleN1.value.replace(",", ".");
-    let nivel2 = controleN2.value.replace(",", ".");
+    let nivel1 = inputControleN1.value.replace(",", ".");
+    let nivel2 = inputControleN2.value.replace(",", ".");
 
-    if(nivel1.trim() === "" || nivel2.trim() === "") {
+    if (nivel1.trim() === "" || nivel2.trim() === "") {
         alert("Por favor, preencha ambos os campos de controle.");
         return;
-    }else if (isNaN(nivel1) || isNaN(nivel2)) {
+    } else if (isNaN(nivel1) || isNaN(nivel2)) {
         alert("Por favor, insira apenas números nos campos de controle.");
         return;
-    }else {
-        listaControleN1.push(nivel1);
-        listaControleN2.push(nivel2);
-        controleN1.value = "";
-        controleN2.value = "";
-      if(listaControleN1.length >= 2 && listaControleN2.length >= 2){
-        calcularMedia(listaControleN1, outputMediaN1);
-        //calcularMedia(listaControleN2, outputMediaN2);
-      }
+    } else {
+        listaControleN1.push(Number(nivel1));
+        listaControleN2.push(Number(nivel2));
+        inputControleN1.value = "";
+        inputControleN2.value = "";
+        if (listaControleN1.length >= 2 && listaControleN2.length >= 2) {
+            calcularMedia(listaControleN1, outputMediaN1);
+            calcularMedia(listaControleN2, outputMediaN2);
+        }
     }
 }
 
 function calcularMedia(listaControle, outputMedia) {
-  let mediaListaControle = listaControle.reduce((acc, valor) => acc + parseFloat(valor), 0) / listaControle.length;
-  mediaListaControle = mediaListaControle.toFixed(2);
-  outputMedia.textContent = mediaListaControle.replace(".", ",");
+    let mediaListaControle = listaControle.reduce((acc, valor) => acc + parseFloat(valor), 0) / listaControle.length;
+    mediaListaControle = mediaListaControle.toFixed(2);
+    outputMedia.textContent = mediaListaControle.replace(".", ",");
     calcularDp(mediaListaControle, listaControle, listaDpN1);
-    //calcularDP(mediaN2, listaControleN2);
+    calcularDP(mediaListaControle, listaControle, listaDpN2);
 }
 
-function calcularDp(media, listaControle, listaDp){
+function calcularDp(media, listaControle, listaDp) {
     listaDp.length = 0;
 
-    for(let i = 0; i < listaControle.length; i++) {
-        let valorDiferenca = (listaControle[i] - media)**2;
+    for (let i = 0; i < listaControle.length; i++) {
+        let valorDiferenca = (listaControle[i] - media) ** 2;
         listaDp.push(valorDiferenca);
     }
-    let mediaDiferenca = listaDp.reduce((acc, i) => acc + i, 0) / (listaDp.length -1);
+    let mediaDiferenca = listaDp.reduce((acc, i) => acc + i, 0) / (listaDp.length - 1);
     let dpFinal = Math.sqrt(mediaDiferenca).toFixed(2);
 
-    if(listaControle.length >= 3){
-      outputDp1N1.textContent = dpFinal.replace(".", ",");
-      outputDp2N1.textContent = (dpFinal * 2).toFixed(2).replace(".", ",");
-      outputDp3N1.textContent = (dpFinal * 3).toFixed(2).replace(".", ","); 
+    if (listaControle.length >= 3) {
+        outputDp1N1.textContent = dpFinal.replace(".", ",");
+        outputDp2N1.textContent = (dpFinal * 2).toFixed(2).replace(".", ",");
+        outputDp3N1.textContent = (dpFinal * 3).toFixed(2).replace(".", ",");
     }
 }
