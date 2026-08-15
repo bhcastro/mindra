21ffed077ef5bf591f9a4cbdb09c13d2a9c21225
Bruno Castro <bruno.hen.castro@gmail.com>
2026-08-15 13:34:48 -0300

    criação do laudo
    Finalizacao e inicio do js

--- evidya/js/laudo-mielo/laudo-mielo.js
+++ evidya/js/laudo-mielo/laudo-mielo.js
@@ -1,2 +1,34 @@
-const material = document.getElementById("material");
-const 
+const dadosPaciente ={
+    sequencia: '',
+    amostra: '',
+    nome: '',
+    dataNascimento: '',
+}
+
+const inputSequencia = document.getElementById('sequencia');
+const inputAmostra = document.getElementById('amostra');
+const inputNome = document.getElementById('nome');
+const inputDataNascimento = document.getElementById('dataNascimento');
+
+const formMedula = document.getElementById('form-medula');
+
+formMedula.addEventListener('submit', (event) => {
+    event.preventDefault();
+    alert('Clicou');
+    carregarDados();
+});
+
+
+function carregarDados() {
+    let sequencia = inputSequencia.value.trim();
+    let amostra = inputAmostra.value.trim();
+    let nome = inputNome.value.trim();
+    let dataNascimento = inputDataNascimento.value;
+
+    dadosPaciente.sequencia = sequencia;
+    dadosPaciente.amostra = amostra;
+    dadosPaciente.nome = nome;
+    dadosPaciente.dataNascimento = dataNascimento;
+
+    console.log(dadosPaciente);
+}
